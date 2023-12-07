ee3f086df4c94a2aa0099ec4d596ac84f55a9522
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-12-07 12:38:09 +0300

    add: redis

---
+++ utils/redis.js
@@ -0,0 +1,37 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log('Redis Client did failed to connect', err);
+      this.isConnectionAlive = false;
+    });
+    this.client.on('connect', () => {
+      this.isConnectionAlive = true;
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
