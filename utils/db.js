cd9c04836eca56ffb3a1b1fd20507c8dd3e0a464
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-12-07 13:17:02 +0300

    add: db

---
+++ utils/db.js
@@ -0,0 +1,31 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const dbInfo = {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 27017,
+      db: process.env.DB_DATABASE || 'files_manager',
+    };
+
+    const url = `mongodb://${dbInfo.host}:${dbInfo.port}/${dbInfo.db}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
