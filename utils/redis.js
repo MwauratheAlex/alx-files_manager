a0987b7537af1af4a410bf498212555937063cc9
Mbugua <mwaurathealex@gmail.com>
2024-02-10 19:40:16 +0300

    redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,7 +1,51 @@
-
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.log(`Unable to create client: ${err}`));
+  }
 
+  /**
+   * Checks whether redis client is alive or dead
+   * @returns {Boolean} true if alive, else false
+   */
+  isAlive() {
+    return this.client.connected;
+  }
+
+  /**
+   * Returns redis value stored for key.
+  * @param {string} key
+  * @returns {string}
+  */
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  /**
+   * Stores a value for a key in redis that expires after duration
+  * @param {string} key
+  * @param {string} value
+  * @param {string} duration
+  */
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, { EX: duration });
+  }
+
+  /**
+   * Removes value for key in redis
+  * @param {string} key
+  */
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
   }
 }
+
+const redisClient = new RedisClient();
+export default redisClient;
