dea6f075d74822e3f9c1c00bff21e02c8febd578
Mbugua <mwaurathealex@gmail.com>
2024-02-11 13:41:13 +0300

    return error objects

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,11 +5,11 @@ class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
 
-    if (!email) return res.status(400).json('Missing email');
-    if (!password) return res.status(400).json('Missing password');
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
 
     const user = await dbClient.collection('users').findOne({ email });
-    if (user) return res.status(400).json('Already exist');
+    if (user) return res.status(400).json({ error: 'Already exist' });
 
     const hashedPassword = sha1(password);
     const newUser = await dbClient.collection('users').insertOne({
