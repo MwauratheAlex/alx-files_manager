68d84e59bc31426f2d0a9a09d2b2076872dfdfa1
Mbugua <mwaurathealex@gmail.com>
2024-02-12 15:15:41 +0300

    json limit

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ import loadRoutes from './routes/index';
 const port = process.env.PORT || 5000;
 const app = express();
 
-loadRoutes(app);
+app.use(express.json({ limit: '100mb' }));
 
-app.use(express.json({ limit: '50mb' }));
+loadRoutes(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
