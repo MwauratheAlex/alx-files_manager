48895450417fa727469163e86c2542f28a2ecd2b
Mbugua <mwaurathealex@gmail.com>
2024-02-12 20:45:21 +0300

    testing2

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,5 +1,17 @@
+import redisClient from '../utils/redis';
+
 class FilesController {
-  static async postUpload(_, res) {
+  static async postUpload(req, res) {
+    const authToken = req.headers['x-token'];
+    if (!authToken) return res.status(401).json({ error: 'Unauthorized' });
+
+    const userId = await redisClient.get(`auth_${authToken}`);
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    const {
+      name,
+    } = req.body;
+    if (!name) return res.status(400).json({ error: 'Missing name' });
     return res.status(200).json('hello world');
   }
 }
