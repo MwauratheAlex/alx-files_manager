9dd04daf866ec1057fd69addf37b13062ab22c80
Mbugua <mwaurathealex@gmail.com>
2024-02-12 15:18:46 +0300

    json limit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import loadRoutes from './routes/index';
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json({ limit: '100mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 loadRoutes(app);
 
